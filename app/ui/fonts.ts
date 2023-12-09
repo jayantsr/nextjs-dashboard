58b5109a5af1f85fc3ebfee4f069d6ae5c20bdac
Jayan <jayantsr@outlook.com>
2023-12-09 12:24:53 +0530

    learning

---
+++ app/ui/fonts.ts
@@ -0,0 +1,18 @@
+/*
+ * File : /Users/jayan/Projects/nextjs-dashboard/app/ui/fonts.ts
+ * Created Date: Saturday, December 9th 2023, 7:46:22 am
+ * Author: Jayan Varma
+ * Author Email : jayantsr@gmail.com
+ * -----
+ * Last Modified: Saturday December 9th 2023 7:46:22 am
+ * -----
+ * Copyright (c) 2023 Cylogics.io
+ *
+ */
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
